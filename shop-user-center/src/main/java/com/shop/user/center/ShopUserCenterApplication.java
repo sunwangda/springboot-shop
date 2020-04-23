6aa86e1c9ff2f3a73e2dc26de37dd238360ae4f0
sunwangda <2451203734@qq.com>
2020-04-23 18:32:33 +0800

    初步结构

--- shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
+++ shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //没有配置数据库得时候写这个可以启动springboot项目
 public class ShopUserCenterApplication {
 
     public static void main(String[] args) {
