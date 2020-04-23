50845139830f20f85dcb3efe03b33ac789605581
sunwangda <2451203734@qq.com>
2020-04-23 16:46:25 +0800

    项目初始化

---
+++ shop-user-center/src/test/java/com/shop/user/center/ShopUserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.user.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
