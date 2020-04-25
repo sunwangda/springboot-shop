e0ef42462d92323b084180b507b03b7aa099e6fe
sunwangda <2451203734@qq.com>
2020-04-25 23:33:56 +0800

    多线程相关配置

--- shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
+++ shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
@@ -1,9 +1,8 @@
 package com.shop.user.center;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //没有配置数据库得时候写这个可以启动springboot项目
