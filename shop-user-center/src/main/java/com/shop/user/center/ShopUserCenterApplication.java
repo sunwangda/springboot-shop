96eb6755e00bcefcd6d05a32b55e6971c0472ccf
sunwangda <2451203734@qq.com>
2020-04-27 02:57:55 +0800

    mybatis-generator功能完善

--- shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
+++ shop-user-center/src/main/java/com/shop/user/center/ShopUserCenterApplication.java
@@ -1,10 +1,12 @@
 package com.shop.user.center;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan({"com.hanfu.user.center.dao"})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //没有配置数据库得时候写这个可以启动springboot项目
 public class ShopUserCenterApplication {
 
